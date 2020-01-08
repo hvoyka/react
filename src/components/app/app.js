309c82987ca40f842c2118f92fe79879a9c839f2
hvoyka <hvoinii@gmail.com>
2020-01-08 17:04:17 +0600

    add item component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,14 +3,15 @@ import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
-import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import TodoList from '../todo-list';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
-
-
 export default class App extends Component{
+    maxId = 100;
+
     state = {
         todoData : [
             {label: 'Drink Coffee', important: false, id: 1},
@@ -32,6 +33,23 @@ export default class App extends Component{
             }
         });
     };
+    addItem = (text) => {
+        const newItem = {
+            label: text,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({ todoData })=>{
+            const newArray = [
+                ...todoData, 
+                newItem
+                ];
+                return {
+                    todoData : newArray
+                }
+        });
+    };
+    
     render(){
         return (
             <div className="todo-app">
@@ -42,6 +60,8 @@ export default class App extends Component{
                 </div>
                 <TodoList todos={this.state.todoData}
                 onDeleted={ this.deleteItem }/>
+                <ItemAddForm
+                onItemAdded={ this.addItem }/>
             </div>
         );
     }
