12d0b1000ef350bc51f5a98976c036439b1aa6aa
hvoyka <hvoinii@gmail.com>
2020-01-08 18:52:48 +0600

    controled component input, reset value after submit

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -13,8 +13,11 @@ export default class ItemAddForm extends Component {
         })
     }
     onSubmit = (e) =>{
-        e.preventDefault();
+        e.preventDefault(); 
         this.props.onItemAdded(this.state.label);
+        this.setState({
+            label: ''
+        });
     }
  
     render(){
@@ -27,6 +30,7 @@ export default class ItemAddForm extends Component {
                 className="form-control"
                 onChange={this.onLabelChange}
                 placeholder="What needs to be done?"
+                value={this.state.label}
                 />
                 <button className="btn btn-outline-secondary"
                 >Add Task</button>
