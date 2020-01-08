a59b88c28660649dbead1ef29c8a6d336b07699a
hvoyka <hvoinii@gmail.com>
2020-01-08 10:01:29 +0600

    add unique keys to items

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import TodoList from './components/todo-list';
 const App = () =>{
 
     const todoData = [
-        {label: 'Drink Coffee', important: false},
-        {label: 'Make Awesom App', important: true},
-        {label: 'Have a lunch', important: false},
+        {label: 'Drink Coffee', important: false, id: 1},
+        {label: 'Make Awesom App', important: true, id: 2},
+        {label: 'Have a lunch', important: false, id: 3},
     ];
     return (
         <div>
