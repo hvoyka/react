c0469a1a138aa3fdb8e891096a15bf1f163dc5f5
hvoyka <hvoinii@gmail.com>
2020-01-08 18:44:07 +0600

    add input for tasks, and func to take text from it

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,14 +4,33 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
 
+    state = {
+        label: ''
+    }
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        })
+    }
+    onSubmit = (e) =>{
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+    }
+ 
     render(){
 
         return (
-            <div className='item-add-form d-flex'>
+            <form className='item-add-form d-flex'
+            onSubmit={this.onSubmit}>
+
+                <input type="text"
+                className="form-control"
+                onChange={this.onLabelChange}
+                placeholder="What needs to be done?"
+                />
                 <button className="btn btn-outline-secondary"
-                onClick={() => this.props.onItemAdded('hello world')}
                 >Add Task</button>
-            </div>
+            </form>
           );
     }
   
