d1faa7520f42e10cf755bb8b10cfb87a7a73c87a
hvoyka <hvoinii@gmail.com>
2020-01-09 20:06:14 +0600

    filter btns component complite

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,8 @@ export default class App extends Component{
             this.createTodoItem('Make Awesom App'),
             this.createTodoItem('Have a lunch')
         ],
-        term: ''
+        term: '',
+        filter: 'all' //active, all, done
     };
     createTodoItem(label) {
          return {
@@ -85,6 +86,9 @@ export default class App extends Component{
     onSearchChange = (term) => {
         this.setState({term});
     }
+    onFilterChange = (filter) => {
+        this.setState({filter});
+    }
 
     search(items, term) {
         if(term.length === 0) {
@@ -96,11 +100,23 @@ export default class App extends Component{
             .indexOf(term.toLowerCase()) > -1;
         })
     }
-   
+    filter(items, filter) {
+        switch(filter) {
+            case 'all' : 
+                return items;
+            case 'active' :
+                return items.filter((item) => !item.done);
+            case 'done' :
+                return items.filter((item) => item.done);
+                default:
+                    return items;
+        };
+    }
     render(){
-        const {todoData, term} = this.state;
+        const {todoData, term, filter} = this.state;
 
-        const visibleItems = this.search(todoData, term);
+
+        const visibleItems = this.filter(this.search(todoData, term), filter);
         const doneCount = todoData
                         .filter((el)=>el.done).length;
         const todoCount = todoData
@@ -111,7 +127,9 @@ export default class App extends Component{
                 <div className="top-panel d-flex">
                     <SearchPanel
                     onSearchChange={this.onSearchChange}/>
-                   <ItemStatusFilter/>
+                   <ItemStatusFilter
+                   filter={filter}
+                   onFilterChange={this.onFilterChange}/>
                 </div>
                 <TodoList todos={visibleItems}
                 onDeleted={ this.deleteItem }
